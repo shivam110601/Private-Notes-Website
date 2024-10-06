e392e4fb6bb7cbbcf6c69107cbdb9886ae45ac22
Shivam Mishra <73604088+shivam110601@users.noreply.github.com>
2024-10-06 14:44:05 +0530

    Minor

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'its awaam till the end'  # securing cookies
+    app.config['SECRET_KEY'] = 'xxxxxxxxx'  # securing cookies
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  # database storage location
     db.init_app(app)  # initializing database for this app
 
